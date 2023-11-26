d704de44b81c6fbba6b23842ed0345ac75f4f706
risingisland <risingisland@yahoo.com>
2023-11-26 20:39:36 +0000

    Update es_ES.php

--- current-events/lang/es_ES.php
+++ current-events/lang/es_ES.php
@@ -3,73 +3,73 @@
 	$i18n = array (
 	
 	# Basics
-		"lang_Menu_Title"		=>	"Current Events 📅",
+		"lang_Menu_Title"		=>	"Eventos Actuales 📅",
 		
-		"lang_Page_Title"		=>	"Current Events",
-		"lang_Description"		=>	"Easy to use calendar plugin ",
+		"lang_Page_Title"		=>	"Eventos Actuales",
+		"lang_Description"		=>	"Complemento de calendario fácil de usar ",
 		
 	# General
-		"lang_BTN_Add_Event"	=>	"Add Event",
-		"lang_BTN_Settings"		=>	"Settings",
-		"lang_BTN_Help"			=>	"Help",
+		"lang_BTN_Add_Event"	=>	"Añadir evento",
+		"lang_BTN_Settings"		=>	"Ajustes",
+		"lang_BTN_Help"			=>	"Ayuda",
 		
-		"lang_BTN_Edit"			=>	"Edit",
-		"lang_BTN_Delete"		=>	"Delete",
+		"lang_BTN_Edit"			=>	"Editar",
+		"lang_BTN_Delete"		=>	"Borrar",
 		
-		"lang_PayPal"			=>	"If you want to support my work via PayPal :) Thanks!",
+		"lang_PayPal"			=>	"Si quieres apoyar mi trabajo a través de PayPal :) ¡Gracias!",
 		
-		"lang_Check_Date"		=>	"Check the Date",
+		"lang_Check_Date"		=>	"Verifique la fecha",
 		
 	# Event
-		"lang_Add_New"			=>	"Add/Edit Event",
-		"lang_Event_Title"		=>	"Titel (Required)",
-		"lang_Event_Description"=>	"Description",
-		"lang_Event_Display"	=>	"Event Display Style",
-		"lang_Event_Full_Day"	=>	"Full Day",
-		"lang_Event_Hourly"		=>	"Hourly",
-		"lang_Event_Date_Start"	=>	"Event Start (Required)",
-		"lang_Event_Date_End"	=>	"Event End (Required)",
-		"lang_Event_Background"	=>	"Event Background Color",
-		"lang_Event_Text"		=>	"Event Text Color",
-		"lang_Event_Link"		=>	"Link to Event (Optional)",
-		"lang_Event_Save"		=>	"Save Event",
+		"lang_Add_New"			=>	"Agregar/Editar Evento",
+		"lang_Event_Title"		=>	"Título (Obligatorio)",
+		"lang_Event_Description"=>	"Descripción",
+		"lang_Event_Display"	=>	"Estilo de Visualización de Eventos",
+		"lang_Event_Full_Day"	=>	"Día completo",
+		"lang_Event_Hourly"		=>	"Por Hora",
+		"lang_Event_Date_Start"	=>	"Inicio del Evento (Obligatorio)",
+		"lang_Event_Date_End"	=>	"Fin del Evento (Obligatorio)",
+		"lang_Event_Background"	=>	"Color de Fondo del Evento",
+		"lang_Event_Text"		=>	"Color del Texto del Evento",
+		"lang_Event_Link"		=>	"Enlace al Evento (Opcional)",
+		"lang_Event_Save"		=>	"Guardar Evento",
 		
 	# Settings
-		"lang_Lang_Shortcode"	=>	"Lang. Shortcode",
+		"lang_Lang_Shortcode"	=>	"Código-Corto de Idioma",
 		
-		"lang_Display_Style"	=>	"Calendar Display Style",
+		"lang_Display_Style"	=>	"Estilo de Visualización del Calendario",
 		
-		"lang_Style_Grid_Month"	=>	"Grid - Month",
-		"lang_Style_Grid_Week"	=>	"Grid - Week",
-		"lang_Style_Grid_Day"	=>	"Grid - Day",
+		"lang_Style_Grid_Month"	=>	"Red - Mes",
+		"lang_Style_Grid_Week"	=>	"Red - Semana",
+		"lang_Style_Grid_Day"	=>	"Red - Día",
 		
-		"lang_Style_List_Year"	=>	"List - Year",
-		"lang_Style_List_Month"	=>	"List - Month",
-		"lang_Style_List_Week"	=>	"List - Week",
-		"lang_Style_List_Day"	=>	"List - Day",
+		"lang_Style_List_Year"	=>	"Lista - Año",
+		"lang_Style_List_Month"	=>	"Lista - Mes",
+		"lang_Style_List_Week"	=>	"Lista - Semana",
+		"lang_Style_List_Day"	=>	"Lista - Día",
 		
-		"lang_Start_Day"		=>	"Start Day",
-		"lang_Monday"			=>	"Monday",
-		"lang_Tuesday"			=>	"Tuesday",
-		"lang_Wednesday"		=>	"Wednesday",
-		"lang_Thursday"			=>	"Thursday",
-		"lang_Friday"			=>	"Friday",
-		"lang_Saturday"			=>	"Saturday",
-		"lang_Sunday"			=>	"Sunday",
+		"lang_Start_Day"		=>	"Día de Inicio",
+		"lang_Monday"			=>	"Lunes",
+		"lang_Tuesday"			=>	"Martes",
+		"lang_Wednesday"		=>	"Miércoles",
+		"lang_Thursday"			=>	"Jueves",
+		"lang_Friday"			=>	"Viernes",
+		"lang_Saturday"			=>	"Sábado",
+		"lang_Sunday"			=>	"Domingo",
 		
-		"lang_Background_Color"	=>	"Default Background Color",
-		"lang_Text_Color"		=>	"Default Text Color",
+		"lang_Background_Color"	=>	"Color de Fondo Predeterminado",
+		"lang_Text_Color"		=>	"Color de Texto Predeterminado",
 		
-		"lang_Header_Nav"		=>	"Show Header Nav,?",
-		"lang_Yes"				=>	"Yes",
+		"lang_Header_Nav"		=>	"¿Mostrar Navegación de Encabezado?",
+		"lang_Yes"				=>	"Si",
 		"lang_No"				=>	"No",
 		
-		"lang_Save"				=>	"Save Settings",
+		"lang_Save"				=>	"Guardar Ajustes",
 		
 	# Help
-		"lang_BTN_Back"			=>	"Back to List",
-		"lang_Shortcode"		=>	"Place this shortcode in content",
-		"lang_Call"				=>	"or this call in your template",
+		"lang_BTN_Back"			=>	"Volver a la Lista",
+		"lang_Shortcode"		=>	"Coloque este código corto en el contenido",
+		"lang_Call"				=>	"o esta llamada en tu plantilla",
 	);
 
 ?>
